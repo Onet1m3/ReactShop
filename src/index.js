31397aa32802435f351631e4c2810fa703613103
Maxim Onetime <def2maxim@yahoo.com>
2021-10-24 23:12:32 +0300

    create first page

--- src/index.js
+++ src/index.js
@@ -1,11 +1,34 @@
 import './main.css'
+
+import React from 'react'
+import ReactDOM from 'react-dom'
 import { applyMiddleware, createStore } from 'redux'
 import { createBrowserHistory } from 'history'
+import thunk from 'redux-thunk' 
+import { ConnectedRouter, routerMiddleware } from 'connected-react-router'
 import { composeWithDevTools } from 'redux-devtools-extension'
+import {Provider} from 'react-redux'
 
-const history = createBrowserHistory
+import createRootReducer from 'reducers'
+import Layout from 'containers/layout'
+
+
+//thunk позволяет делать асинхронные функции
+//applyMiddleware используется для применения всех middleware
+//composeWithDevTools объединяет наши миддлвары для devtools
+
+const history = createBrowserHistory()
+const middlewares = [thunk, routerMiddleware(history)]
 const store = createStore(
     createRootReducer(history),
     composeWithDevTools(applyMiddleware(...middlewares))
 )
-document.write('Hello React/Redux!')
+
+ReactDOM.render(
+    <Provider store={store}>
+        <ConnectedRouter history={history}>
+            <Layout />
+        </ConnectedRouter>
+    </Provider>,
+    document.getElementById('root')
+);
